70f9819077508c874bc861e25021ff11fefefe5a
Alessandro Gomes <alllessandrogomes@gmail.com>
2024-11-24 21:33:51 -0300

    :tada:init: primeiro commit.

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,75 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+
+import { useState } from "react";
+import styles from "./page.module.scss"
+import { parse } from "json2csv";
+import "normalize.css";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  const [breakdowns, setBreakdowns] = useState([])
+  const [code, setCode] = useState("")
+  const [desc, setDesc] = useState("")
+  const [qtd, setQtd] = useState("")
+
+  function addBreakdown(code, desc, qtd, event) {
+    event.preventDefault()
+
+    const breakdown = {
+      codigo: code,
+      descricao: desc.toUpperCase(),
+      quantidade: qtd.toUpperCase()
+    }
+
+    setBreakdowns(prevState => [...prevState, breakdown])
+    setCode("")
+    setDesc("")
+    setQtd("")
+  }
+
+  function downloadCsv() {
+    try {
+      const csv = parse(breakdowns)
+      const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' })
+      const url = window.URL.createObjectURL(blob)
+      const link = document.createElement('a')
+      link.href = url
+      link.setAttribute('download', "avarias.csv")
+      document.body.appendChild(link)
+      link.click()
+      document.body.removeChild(link)
+    } catch (err) {
+      console.error('Erro ao gerar CSV', err)
+    }
+  }
+
+  return (
+    <main className={styles.main}>
+      <h1>Controle de Avarias</h1>
+
+      <form className={styles.form} onSubmit={(event) => addBreakdown(code, desc, qtd, event)}>
+        <div>
+          <label>Código</label>
+          <input type="number" onChange={(event) => setCode(event.target.value)} value={code} required />
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div>
+          <label>Descrição</label>
+          <input type="text" onChange={(event) => setDesc(event.target.value)} value={desc} required />
+        </div>
+        <div>
+          <label>Qtd</label>
+          <input type="text" onChange={(event) => setQtd(event.target.value)} value={qtd} required />
+        </div>
+
+        <button type="submit">Adicionar</button>
+      </form>
+
+      <div>
+        <h2>Avarias adicionadas</h2>
+        {breakdowns.length ? breakdowns.map(item => <p key={item.codigo}>{item.codigo} | {item.descricao} | {item.quantidade}</p>) : <p>Nenhuma avaria adicionada</p>}
+      </div>
+
+      <button disabled={!breakdowns.length} onClick={downloadCsv}>Baixar CSV</button>
+    </main>
   );
 }
